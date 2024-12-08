ce82a5bfeb02984148572123ce30333c05625939
gopikrishnabrahmarouthu <131034055+gopikrishnabrahmarouthu@users.noreply.github.com>
2024-12-08 21:05:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,3 @@
 module "aws-s3" {
   source = "./aws-s3"
-
-  bucket = "module.aws-s3.bucket"
-  acl    = "private"
-
-  control_object_ownership = true
-  object_ownership         = "ObjectWriter"
-
-  versioning = {
-    enabled = true
-  }
 }
